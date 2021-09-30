0bac1e7d3bfaad17ec1633face5e98334a8811d1
Chafik Amraoui <chafikamraoui0@gmail.com>
2021-09-30 20:09:37 +0100

    add product and about pages

--- js/main.js
+++ js/main.js
@@ -38,4 +38,53 @@
 
 $('.checkbtn').click(function() {
     $("body").toggleClass("fixedPosition");
-})
+});
+
+
+
+// $(function() {
+//     $('#small-thumbs li').on('click', function() {
+//         let _img = $(this).find('img').attr('src');
+//         console.log(_img);
+//         $('#thumbmail').attr('src', _img);
+
+
+
+//     })
+// })
+
+
+$(document).ready(function() {
+
+    var quantitiy = 0;
+    $('.quantity-right-plus').click(function(e) {
+
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+
+        // If is not undefined
+
+        $('#quantity').val(quantity + 1);
+
+
+        // Increment
+
+    });
+
+    $('.quantity-left-minus').click(function(e) {
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+
+        // If is not undefined
+
+        // Increment
+        if (quantity > 0) {
+            $('#quantity').val(quantity - 1);
+        }
+    });
+
+});
